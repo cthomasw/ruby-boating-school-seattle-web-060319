6125e9e1b896abdb52b429ee7c48271c8013b6d8
Claire Wilbert <claire.wilbert@gmail.com>
2019-06-08 16:51:25 -0700

    wrote out methods for Instructor, Student, and BoatingTest classes

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,41 @@
+require 'pry'
+
 class Instructor
 
+	attr_accessor :name, :add_boating_test
+
+	@@all = []
+
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def pass_student(student, boating_test_name)
+		all_tests = BoatingTest.all.select { |test| test == boating_test_name && test.student == student}
+	
+		if all_tests.count != 0
+			#binding.pry
+			all_tests[0].boating_test_status = "passed"
+
+		else BoatingTest.new(student, boating_test_name, "passed", self)
+		end
+	end
+
+	def fail_student(student, boating_test_name)
+		all_tests = BoatingTest.all.select { |test| test == boating_test_name && test.student == student}
+
+		if all_tests.count != 0
+			all_tests[0].boating_test_status = "failed"
+		else BoatingTest.new(student, boating_test_name, "failed", self)
+		end
+	end
+
 end
+
+
+
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,35 @@
 class Student
 
+	attr_accessor :first_name
+
+	@@all = []
+
+	def initialize(first_name)
+		@first_name = first_name
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def add_boating_test(boating_test_name, boating_test_status, instructor)
+		BoatingTest.new(self, boating_test_name, boating_test_status, instructor)
+	end
+
+	def find_student(first_name)
+		Student.all.select { |student| student.first_name == first_name}
+	end
+
+	def grade_percentage
+		passed_count = 0
+		total_tests = BoatingTest.all.select { |test| test.student == self }
+		total_tests.select do |test|
+			if test.boating_test_status == "passed"
+				passed_count += 1.to_f
+			end
+		end
+		passed_count / total_tests.count.to_f
+	end
+
 end
